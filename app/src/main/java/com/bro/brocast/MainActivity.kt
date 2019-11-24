78319dd4928dfebf6bac4fce012e57839cf92cec
Grabot <SanderKools@gmail.com>
2019-11-24 14:48:02 +0100

    added some encryption with some tests Will use it to encrypt passwords

--- app/src/main/java/com/bro/brocast/MainActivity.kt
+++ app/src/main/java/com/bro/brocast/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.bro.brocast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.JsonReader
+import com.beust.klaxon.Parser
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import se.simbio.encryption.Encryption
+import java.io.StringReader
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,11 +42,20 @@ class MainActivity : AppCompatActivity() {
                 })
         }
 
+        val key = "toBeDetermined"
+        val salt = "verySalty"
+        val iv = ByteArray(16)
+        val encryption: Encryption = Encryption.getDefault(key, salt, iv)
+
+        val username = "Sander30name"
+        val password = "Sander30pass"
+        val encrypted = encryption.encryptOrNull(password)
+
         buttonRegister.setOnClickListener {
             println("A user wants to register!")
             BroCastAPI
                 .service
-                .registerUser("Sander3", "Sander3")
+                .registerUser(username, encrypted)
                 .enqueue(object : Callback<ResponseBody> {
                     override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                         println("An exception occured with the GET call:: " + t.message)
@@ -48,8 +64,17 @@ class MainActivity : AppCompatActivity() {
                     override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
                         if (response.isSuccessful) {
                             val msg = response.body()?.string()
-                            println("The GET message returned from the server:: " + msg)
-                            Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
+                            if (msg != null) {
+                                val parser: Parser = Parser.default()
+                                val stringBuilder: StringBuilder = StringBuilder(msg)
+                                val json: JsonObject = parser.parse(stringBuilder) as JsonObject
+                                val decrypted = encryption.decryptOrNull(json.get("password").toString())
+                                println("The GET message returned from the server:: " + msg)
+                                Toast.makeText(applicationContext,
+                                    "user " + json.get("username") + " signed up with password: " + decrypted +
+                                            " \n" + json.get("messgae"),
+                                    Toast.LENGTH_SHORT).show()
+                            }
                         }
                     }
                 })
