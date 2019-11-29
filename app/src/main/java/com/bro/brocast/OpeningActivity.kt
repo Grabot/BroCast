a7b70c52b96527e9ea4fefc1e13587f162a097dc
Sander Kools <sanderkools@gmail.com>
2019-11-29 17:12:52 +0100

    fixed the icon issues and added a new main screen

---
+++ app/src/main/java/com/bro/brocast/OpeningActivity.kt
@@ -0,0 +1,12 @@
+package com.bro.brocast
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OpeningActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.opening_screen)
+    }
+}
