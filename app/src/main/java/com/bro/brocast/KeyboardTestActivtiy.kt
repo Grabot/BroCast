594542625204c217c2abe71e02aa02ee0b2f9188
Sander Kools <sanderkools@gmail.com>
2019-12-11 12:49:34 +0100

    a basic custom keyboard is working

--- app/src/main/java/com/bro/brocast/KeyboardTestActivtiy.kt
+++ app/src/main/java/com/bro/brocast/KeyboardTestActivtiy.kt
@@ -1,27 +1,31 @@
 package com.bro.brocast
 
+import android.os.Build
 import android.os.Bundle
 import android.text.InputType
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.bro.brocast.objects.MyKeyboard
 
 
 class KeyboardTestActivtiy: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-//        https://stackoverflow.com/questions/9577304/how-can-you-make-a-custom-keyboard-in-android/45005691#45005691
         super.onCreate(savedInstanceState)
         setContentView(R.layout.test)
 
         val editText = findViewById(R.id.editText) as EditText
         val keyboard = findViewById(R.id.keyboard) as MyKeyboard
 
-        // prevent system keyboard from appearing when EditText is tapped
         editText.setRawInputType(InputType.TYPE_CLASS_TEXT)
         editText.setTextIsSelectable(true)
+        // TODO @Skools: set the minimum SDK to this version (LOLLIPOP).
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            editText.requestFocus()
+            editText.showSoftInputOnFocus = false
+        }
 
-        // pass the InputConnection from the EditText to the keyboard
         val ic = editText.onCreateInputConnection(EditorInfo())
         keyboard.setInputConnection(ic)
     }
---
+++ app/src/main/java/com/bro/brocast/objects/MyKeyboard.kt
@@ -0,0 +1,59 @@
+package com.bro.brocast.objects
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputConnection
+import android.widget.Button
+import android.widget.LinearLayout
+import com.bro.brocast.R
+
+
+class MyKeyboard: LinearLayout {
+
+    constructor(context: Context) : super(context){
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet):    super(context, attrs){
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?,    defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context)
+    }
+
+    var button1: Button? = null
+    var button2: Button? = null
+
+//    private val keyValues = SparseArray<String>()
+    private var inputConnection: InputConnection? = null
+
+
+    fun init(context: Context) {
+        LayoutInflater.from(context).inflate(R.layout.keyboard, this, true)
+        button1 = findViewById(R.id.button_1) as Button
+        button1!!.setOnClickListener(clickButtonListener)
+        button2 = findViewById(R.id.button_2) as Button
+        button2!!.setOnClickListener(clickButtonListener)
+
+//        keyValues.put(R.id.button_1, "1")
+//        keyValues.put(R.id.button_2, "2")
+    }
+
+    private val clickButtonListener = View.OnClickListener { view ->
+        when (view.getId()) {
+            R.id.button_1 -> {
+                inputConnection!!.commitText("1", 1)
+            }
+            R.id.button_2 -> {
+                inputConnection!!.commitText("2", 1)
+            }
+        }
+    }
+
+    fun setInputConnection(ic: InputConnection) {
+        inputConnection = ic
+    }
+}
