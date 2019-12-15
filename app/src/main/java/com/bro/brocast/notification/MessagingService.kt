168a4fc3c034d3cc179ae14b101f332fd980e5a9
Grabot <SanderKools@gmail.com>
2019-12-15 23:36:19 +0100

    wrapping up the notification with a random message icon

--- app/src/main/java/com/bro/brocast/notification/MessagingService.kt
+++ app/src/main/java/com/bro/brocast/notification/MessagingService.kt
@@ -8,32 +8,25 @@ import com.google.firebase.messaging.RemoteMessage
 class MessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         println("From: " + remoteMessage.from)
-        // Check if message contains a data payload.
-        // TODO @Sander: We only use notifications, so possibly remove this part.
-        if (remoteMessage.data.size > 0) {
-            println("Message data payload: " + remoteMessage.data)
-            if ( /* Check if data needs to be processed by long running job */true) { // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                println("schedule job")
-//                scheduleJob()
-            } else { // Handle message within 10 seconds
-                println("handle now")
-//                handleNow()
-            }
-        }
-        // Check if message contains a notification payload.
+
         if (remoteMessage.notification != null) {
             println("Message notificiation body: " + remoteMessage.notification!!.body)
-            showNotification(remoteMessage.notification?.title, remoteMessage.notification?.body)
+            showNotification(remoteMessage.notification?.title!!, remoteMessage.notification?.body!!)
         }
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
 
-    private fun showNotification(title: String?, body: String?) {
-        println("show notification")
+    private fun showNotification(title: String, body: String) {
+        // TODO @Sander: Not sure, but this is not called when the app is not open. Make sure this
+        //  gives the same notification or something else or don't show it when the app is open
+//        NotificationUtil.createTestNotification(
+//            this,
+//            "test1",
+//            "test2",
+//            "test3",
+//            false
+//        )
+//        println("show notification")
     }
 
     /**
@@ -42,16 +35,9 @@ class MessagingService : FirebaseMessagingService() {
      * is initially generated so this is where you would retrieve the token.
      */
     override fun onNewToken(token: String) {
-        println("Refresed token: $token")
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-        println("send registration")
-        // TODO @Sander: Here you should send the token to your database. This token can be used to
-        //  determine the correct user. Since the user is not created when the app is opened you
-        //  should store the token first and send it allong with the user registration. Since the
-        //  user can close the app before first creating it's account you should store the token in
-        //  sharedpreferences.
         val sharedPreferences = this.getSharedPreferences(this.getString(R.string.preference_file_key), Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         editor.putString("REGISTRATION_TOKEN", token)
