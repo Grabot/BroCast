86183d9fa0642d095fcb7608db4ff0b8fd90cccf
Grabot <SanderKools@gmail.com>
2019-12-18 23:34:02 +0100

    made a start with adding an emotion to the bro

--- app/src/main/java/com/bro/brocast/RegisterActivity.kt
+++ app/src/main/java/com/bro/brocast/RegisterActivity.kt
@@ -1,11 +1,23 @@
 package com.bro.brocast
 
+import android.app.Activity
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.beust.klaxon.JsonObject
 import com.bro.brocast.api.RegisterAPI
+import com.bro.brocast.api.SendMessagesAPI
+import com.bro.brocast.objects.MyKeyboard
+import kotlinx.android.synthetic.main.activity_messaging.*
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.keyboard
 import se.simbio.encryption.Encryption
 
 class RegisterActivity : AppCompatActivity() {
@@ -13,6 +25,9 @@ class RegisterActivity : AppCompatActivity() {
     var encryption: Encryption? = null
     // A simple variable to lock the register button after it's pressed
     private var pressedRegister = false
+
+    var bromotion: EditText? = null
+    var broName: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +47,26 @@ class RegisterActivity : AppCompatActivity() {
 
         buttonRegisterBro.setOnClickListener(clickRegisterListener)
         pressedRegister = false
+
+        bromotion = findViewById(R.id.broNameRegisterEmotion) as EditText
+        broName = findViewById(R.id.broNameRegister) as EditText
+        val keyboard = findViewById(R.id.keyboard) as MyKeyboard
+
+        bromotion!!.setOnClickListener(clickRegisterListener)
+        broName!!.setOnClickListener(clickRegisterListener)
+
+        bromotion!!.setRawInputType(InputType.TYPE_CLASS_TEXT)
+        bromotion!!.setTextIsSelectable(true)
+        bromotion!!.setTextSize(20f)
+        // TODO @Skools: set the minimum SDK to this version (LOLLIPOP).
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            bromotion!!.requestFocus()
+            bromotion!!.showSoftInputOnFocus = false
+        }
+
+        val ic = bromotion!!.onCreateInputConnection(EditorInfo())
+        keyboard.setInputConnection(ic)
+
     }
 
     private val clickRegisterListener = View.OnClickListener { view ->
@@ -44,6 +79,18 @@ class RegisterActivity : AppCompatActivity() {
                     val passwordEncrypt = encryption!!.encryptOrNull(password)
                     println("bro $broName wants to register!")
                     RegisterAPI.registerBro(broName, passwordEncrypt, this@RegisterActivity, applicationContext)
+                }
+            }
+            R.id.broMessageField -> {
+                // We want to make the keyboard visible if it isn't yet.
+                if (keyboard.visibility != View.VISIBLE) {
+                    keyboard.visibility = View.VISIBLE
+                }
+            }
+            R.id.broNameRegister -> {
+                // The user clicked on the other field so we make the emotion keyboard invisible
+                if (keyboard.visibility == View.VISIBLE) {
+                    keyboard.visibility = View.INVISIBLE
                 }
             }
         }
