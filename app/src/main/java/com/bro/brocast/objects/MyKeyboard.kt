5adf44c260ca17118553587c9e47839025eb08df
Grabot <SanderKools@gmail.com>
2019-12-23 00:02:47 +0100

    upgraded notification sound and improved keyboard

--- app/src/main/java/com/bro/brocast/objects/MyKeyboard.kt
+++ app/src/main/java/com/bro/brocast/objects/MyKeyboard.kt
@@ -29,6 +29,7 @@ class MyKeyboard: LinearLayout {
     var buttonWink: Button? = null
     var buttonThrowingAKiss: Button? = null
     var buttonBack: Button? = null
+
     var buttonEggplant: Button? = null
     var buttonBanana: Button? = null
     var buttonTearsOfJoy: Button? = null
@@ -37,8 +38,10 @@ class MyKeyboard: LinearLayout {
     val emoji_Smile = 0x1F604
     val emoji_Wink = 0x1F609
     val emoji_throwing_a_kiss = 0x1F618
+
     val emoji_eggplant = 0x1F346
     val emoji_banana = 0x1F34C
+
     val emoji_tears_of_joy = 0x1F602
     val emoji_heart_shaped_eyes = 0x1F60D
 
@@ -56,20 +59,25 @@ class MyKeyboard: LinearLayout {
         buttonThrowingAKiss!!.setOnClickListener(clickButtonListener)
         buttonBack = findViewById(R.id.button_back) as Button
         buttonBack!!.setOnClickListener(clickButtonListener)
+
         buttonEggplant = findViewById(R.id.button_eggplant) as Button
         buttonEggplant!!.setOnClickListener(clickButtonListener)
         buttonBanana = findViewById(R.id.button_banana) as Button
         buttonBanana!!.setOnClickListener(clickButtonListener)
+
         buttonTearsOfJoy = findViewById(R.id.button_tears_of_joy)
         buttonTearsOfJoy!!.setOnClickListener(clickButtonListener)
         buttonHeartShapedEyes = findViewById(R.id.button_heart_shaped_eyes)
         buttonHeartShapedEyes!!.setOnClickListener(clickButtonListener)
 
+
         buttonSmile!!.setText(getEmojiByUnicode(emoji_Smile))
         buttonWink!!.setText(getEmojiByUnicode(emoji_Wink))
         buttonThrowingAKiss!!.setText(getEmojiByUnicode(emoji_throwing_a_kiss))
+        
         buttonEggplant!!.setText(getEmojiByUnicode(emoji_eggplant))
         buttonBanana!!.setText(getEmojiByUnicode(emoji_banana))
+
         buttonTearsOfJoy!!.setText(getEmojiByUnicode(emoji_tears_of_joy))
         buttonHeartShapedEyes!!.setText(getEmojiByUnicode(emoji_heart_shaped_eyes))
     }
