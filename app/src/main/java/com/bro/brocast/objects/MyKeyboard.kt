84a0d7b78466d2f772c950fb23a03b7a5a565780
Sander Kools <sanderkools@gmail.com>
2019-12-23 14:38:14 +0100

    improving the keyboard

--- app/src/main/java/com/bro/brocast/objects/MyKeyboard.kt
+++ app/src/main/java/com/bro/brocast/objects/MyKeyboard.kt
@@ -25,61 +25,146 @@ class MyKeyboard: LinearLayout {
         init(context)
     }
 
+    var buttonBack: Button? = null
+    // button row 1
     var buttonSmile: Button? = null
     var buttonWink: Button? = null
     var buttonThrowingAKiss: Button? = null
-    var buttonBack: Button? = null
+    var buttonKissingFaceClosedEyes: Button? = null
+    var buttonStuckOutTongue: Button? = null
+    var buttonColdSweat: Button? = null
+    var buttonPensive: Button? = null
 
+    // button row 2
     var buttonEggplant: Button? = null
     var buttonBanana: Button? = null
-    var buttonTearsOfJoy: Button? = null
-    var buttonHeartShapedEyes: Button? = null
+    var buttonHeart: Button? = null
+    var buttonRollingOnTheFloor: Button? = null
+    var buttonFaceWithHearts: Button? = null
+    var buttonFoldedHands: Button? = null
+    var buttonLoudlyCrying: Button? = null
+    var buttonRightFacingFist: Button? = null
+    var buttonLeftFacingFist: Button? = null
 
+    // button row 3
+    var buttonTearsOfJoy: Button? = null
+    var buttonSweatDroplets: Button? = null
+    var buttonHeartShapedEyes: Button? = null
+    var buttonThumbsUp: Button? = null
+    var buttonExlamationMark: Button? = null
+    var buttonQuestionMark: Button? = null
+
+    // emoji row 1
     val emoji_Smile = 0x1F604
     val emoji_Wink = 0x1F609
     val emoji_throwing_a_kiss = 0x1F618
+    val emoji_kissing_face_closed_eyes = 0x1F61A
+    val emoji_stuck_out_tongue = 0x1F61B
+    val emoji_cold_sweat = 0x1F613
+    val emoji_pensive = 0x1F614
 
+    // emoji row 2
     val emoji_eggplant = 0x1F346
     val emoji_banana = 0x1F34C
+    val emoji_heart = 0x2764
+    val emoji_rolling_on_the_floor = 0x1F923
+    val emoji_face_with_hearts = 0x1F970
+    val emoji_folded_hands = 0x1F64F
+    val emoji_loudly_crying = 0x1F62D
+    val emoji_right_facing_fist = 0x1F91C
+    val emoji_left_facing_fist = 0x1F91B
 
+    // emoji row 3
     val emoji_tears_of_joy = 0x1F602
+    val emoji_sweat_droplets = 0x1F4A6
     val emoji_heart_shaped_eyes = 0x1F60D
+    val emoji_thumbs_up = 0x1F44D
 
-//    private val keyValues = SparseArray<String>()
     private var inputConnection: InputConnection? = null
 
 
     fun init(context: Context) {
         LayoutInflater.from(context).inflate(R.layout.keyboard, this, true)
-        buttonSmile = findViewById(R.id.button_smile) as Button
+
+        buttonBack = findViewById(R.id.button_back)
+        buttonBack!!.setOnClickListener(clickButtonListener)
+        // row 1
+        buttonSmile = findViewById(R.id.button_smile)
         buttonSmile!!.setOnClickListener(clickButtonListener)
-        buttonWink = findViewById(R.id.button_wink) as Button
+        buttonWink = findViewById(R.id.button_wink)
         buttonWink!!.setOnClickListener(clickButtonListener)
         buttonThrowingAKiss = findViewById(R.id.button_throwing_a_kiss)
         buttonThrowingAKiss!!.setOnClickListener(clickButtonListener)
-        buttonBack = findViewById(R.id.button_back) as Button
-        buttonBack!!.setOnClickListener(clickButtonListener)
+        buttonKissingFaceClosedEyes = findViewById(R.id.button_kissing_face_closed_eyes)
+        buttonKissingFaceClosedEyes!!.setOnClickListener(clickButtonListener)
+        buttonStuckOutTongue = findViewById(R.id.button_stuck_out_tongue)
+        buttonStuckOutTongue!!.setOnClickListener(clickButtonListener)
+        buttonColdSweat = findViewById(R.id.button_cold_sweat)
+        buttonColdSweat!!.setOnClickListener(clickButtonListener)
+        buttonPensive = findViewById(R.id.button_pensive)
+        buttonPensive!!.setOnClickListener(clickButtonListener)
 
-        buttonEggplant = findViewById(R.id.button_eggplant) as Button
-        buttonEggplant!!.setOnClickListener(clickButtonListener)
-        buttonBanana = findViewById(R.id.button_banana) as Button
-        buttonBanana!!.setOnClickListener(clickButtonListener)
-
+        // row 2
         buttonTearsOfJoy = findViewById(R.id.button_tears_of_joy)
         buttonTearsOfJoy!!.setOnClickListener(clickButtonListener)
         buttonHeartShapedEyes = findViewById(R.id.button_heart_shaped_eyes)
         buttonHeartShapedEyes!!.setOnClickListener(clickButtonListener)
+        buttonHeart = findViewById(R.id.button_heart)
+        buttonHeart!!.setOnClickListener(clickButtonListener)
+        buttonRollingOnTheFloor = findViewById(R.id.button_rolling_on_the_floor)
+        buttonRollingOnTheFloor!!.setOnClickListener(clickButtonListener)
+        buttonFaceWithHearts = findViewById(R.id.button_face_with_hearts)
+        buttonFaceWithHearts!!.setOnClickListener(clickButtonListener)
+        buttonFoldedHands = findViewById(R.id.button_folded_hands)
+        buttonFoldedHands!!.setOnClickListener(clickButtonListener)
+        buttonLoudlyCrying = findViewById(R.id.button_loudly_crying)
+        buttonLoudlyCrying!!.setOnClickListener(clickButtonListener)
+        buttonRightFacingFist = findViewById(R.id.button_right_facing_fist)
+        buttonRightFacingFist!!.setOnClickListener(clickButtonListener)
+        buttonLeftFacingFist = findViewById(R.id.button_left_facing_fist)
+        buttonLeftFacingFist!!.setOnClickListener(clickButtonListener)
+
+        // row 3
+        buttonEggplant = findViewById(R.id.button_eggplant)
+        buttonEggplant!!.setOnClickListener(clickButtonListener)
+        buttonSweatDroplets = findViewById(R.id.button_sweat_droplets)
+        buttonSweatDroplets!!.setOnClickListener(clickButtonListener)
+        buttonBanana = findViewById(R.id.button_banana)
+        buttonBanana!!.setOnClickListener(clickButtonListener)
+        buttonThumbsUp = findViewById(R.id.button_thumbs_up)
+        buttonThumbsUp!!.setOnClickListener(clickButtonListener)
+        buttonExlamationMark = findViewById(R.id.button_exlamation_mark)
+        buttonExlamationMark!!.setOnClickListener(clickButtonListener)
+        buttonQuestionMark = findViewById(R.id.button_question_mark)
+        buttonQuestionMark!!.setOnClickListener(clickButtonListener)
 
 
-        buttonSmile!!.setText(getEmojiByUnicode(emoji_Smile))
-        buttonWink!!.setText(getEmojiByUnicode(emoji_Wink))
-        buttonThrowingAKiss!!.setText(getEmojiByUnicode(emoji_throwing_a_kiss))
-        
-        buttonEggplant!!.setText(getEmojiByUnicode(emoji_eggplant))
-        buttonBanana!!.setText(getEmojiByUnicode(emoji_banana))
+        // row 1
+        buttonSmile!!.text = getEmojiByUnicode(emoji_Smile)
+        buttonWink!!.text = getEmojiByUnicode(emoji_Wink)
+        buttonThrowingAKiss!!.text = getEmojiByUnicode(emoji_throwing_a_kiss)
+        buttonKissingFaceClosedEyes!!.text = getEmojiByUnicode(emoji_kissing_face_closed_eyes)
+        buttonStuckOutTongue!!.text = getEmojiByUnicode(emoji_stuck_out_tongue)
+        buttonColdSweat!!.text = getEmojiByUnicode(emoji_cold_sweat)
+        buttonPensive!!.text = getEmojiByUnicode(emoji_pensive)
 
-        buttonTearsOfJoy!!.setText(getEmojiByUnicode(emoji_tears_of_joy))
-        buttonHeartShapedEyes!!.setText(getEmojiByUnicode(emoji_heart_shaped_eyes))
+        // row 2
+        buttonTearsOfJoy!!.text = getEmojiByUnicode(emoji_tears_of_joy)
+        buttonHeartShapedEyes!!.text = getEmojiByUnicode(emoji_heart_shaped_eyes)
+        buttonHeart!!.text = getEmojiByUnicode(emoji_heart)
+        buttonRollingOnTheFloor!!.text = getEmojiByUnicode(emoji_rolling_on_the_floor)
+        buttonFoldedHands!!.text = getEmojiByUnicode(emoji_folded_hands)
+        buttonLoudlyCrying!!.text = getEmojiByUnicode(emoji_loudly_crying)
+        buttonRightFacingFist!!.text = getEmojiByUnicode(emoji_right_facing_fist)
+        buttonLeftFacingFist!!.text = getEmojiByUnicode(emoji_left_facing_fist)
+
+        // row 3
+        buttonEggplant!!.text = getEmojiByUnicode(emoji_eggplant)
+        buttonSweatDroplets!!.text = getEmojiByUnicode(emoji_sweat_droplets)
+        buttonBanana!!.text = getEmojiByUnicode(emoji_banana)
+        buttonThumbsUp!!.text = getEmojiByUnicode(emoji_thumbs_up)
+        buttonExlamationMark!!.text = "!"
+        buttonQuestionMark!!.text = "?"
     }
 
     private val clickButtonListener = OnClickListener { view ->
@@ -89,11 +174,21 @@ class MyKeyboard: LinearLayout {
 
                 if (TextUtils.isEmpty(selectedText)) {
                     // We assume that the emoji will always have length 2
-                    inputConnection!!.deleteSurroundingText(2, 0)
+                    // TODO @Skools: sometimes the emoji is NOT length 2. Fix this!
+                    //  Possibly a complete Emoji overhaul
+                    if (inputConnection!!.getTextBeforeCursor(1, 1).toString().equals("❤")
+                        || inputConnection!!.getTextBeforeCursor(1, 1).toString().equals("!")
+                        || inputConnection!!.getTextBeforeCursor(1, 1).toString().equals("?")) {
+                        // Simple solution to fix the emoji's and characters of length 1
+                        inputConnection!!.deleteSurroundingText(1, 0)
+                    } else {
+                        inputConnection!!.deleteSurroundingText(2, 0)
+                    }
                 } else {
                     inputConnection!!.commitText("", 1)
                 }
             }
+            // row 1
             R.id.button_smile -> {
                 inputConnection!!.commitText(getEmojiByUnicode(emoji_Smile), 1)
             }
@@ -103,17 +198,64 @@ class MyKeyboard: LinearLayout {
             R.id.button_throwing_a_kiss -> {
                 inputConnection!!.commitText(getEmojiByUnicode(emoji_throwing_a_kiss), 1)
             }
-            R.id.button_eggplant -> {
-                inputConnection!!.commitText(getEmojiByUnicode(emoji_eggplant), 1)
+            R.id.button_kissing_face_closed_eyes -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_kissing_face_closed_eyes), 1)
             }
-            R.id.button_banana -> {
-                inputConnection!!.commitText(getEmojiByUnicode(emoji_banana), 1)
+            R.id.button_stuck_out_tongue -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_stuck_out_tongue), 1)
             }
+            R.id.button_cold_sweat -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_cold_sweat), 1)
+            }
+            R.id.button_pensive -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_pensive), 1)
+            }
+            // row 2
             R.id.button_tears_of_joy -> {
                 inputConnection!!.commitText(getEmojiByUnicode(emoji_tears_of_joy), 1)
             }
             R.id.button_heart_shaped_eyes -> {
                 inputConnection!!.commitText(getEmojiByUnicode(emoji_heart_shaped_eyes), 1)
+            }
+            R.id.button_heart -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_heart), 1)
+            }
+            R.id.button_rolling_on_the_floor -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_rolling_on_the_floor), 1)
+            }
+            R.id.button_face_with_hearts -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_face_with_hearts), 1)
+            }
+            R.id.button_folded_hands -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_folded_hands), 1)
+            }
+            R.id.button_loudly_crying -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_loudly_crying), 1)
+            }
+            R.id.button_right_facing_fist -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_right_facing_fist), 1)
+            }
+            R.id.button_left_facing_fist -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_left_facing_fist), 1)
+            }
+            // row 3
+            R.id.button_eggplant -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_eggplant), 1)
+            }
+            R.id.button_sweat_droplets -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_sweat_droplets), 1)
+            }
+            R.id.button_banana -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_banana), 1)
+            }
+            R.id.button_thumbs_up -> {
+                inputConnection!!.commitText(getEmojiByUnicode(emoji_thumbs_up), 1)
+            }
+            R.id.button_exlamation_mark -> {
+                inputConnection!!.commitText("!", 1)
+            }
+            R.id.button_question_mark -> {
+                inputConnection!!.commitText("?", 1)
             }
         }
     }
