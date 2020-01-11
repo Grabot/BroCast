255e9cc0f4ffa17ca215f74a72c2dcd8026fd793
Grabot <SanderKools@gmail.com>
2020-01-11 21:57:44 +0100

    added some more emojis to the first keyboard

--- app/src/main/java/com/bro/brocast/keyboards/FirstKeyboard.kt
+++ app/src/main/java/com/bro/brocast/keyboards/FirstKeyboard.kt
@@ -10,7 +10,7 @@ import android.widget.LinearLayout
 import com.bro.brocast.R
 import com.bro.brocast.R.id.button_grinning_face
 import com.bro.brocast.R.id.button_smiling_face_with_open_mouth
-import com.bro.brocast.R.id.button_smiling_face_with_open_mouth_and_smilinge_eyes
+import com.bro.brocast.R.id.button_smiling_face_with_open_mouth_and_smiling_eyes
 import com.bro.brocast.R.id.button_grinning_face_with_smiling_eyes
 import com.bro.brocast.R.id.button_smiling_face_with_open_mouth_and_closed_eyes
 import com.bro.brocast.R.id.button_smiling_face_with_open_mouth_and_cold_sweat
@@ -27,6 +27,25 @@ import com.bro.brocast.R.id.button_smiling_face_with_heart_eyes
 import com.bro.brocast.R.id.button_smiling_face_with_3_hearts
 import com.bro.brocast.R.id.button_face_blowing_a_kiss
 import com.bro.brocast.R.id.button_kissing_face
+import com.bro.brocast.R.id.button_kissing_face_with_smiling_eyes
+import com.bro.brocast.R.id.button_kissing_face_with_closed_eyes
+import com.bro.brocast.R.id.button_face_savouring_delicious_food
+import com.bro.brocast.R.id.button_face_with_stuck_out_tongue
+import com.bro.brocast.R.id.button_face_with_stuck_out_tongue_and_closed_eyes
+import com.bro.brocast.R.id.button_face_with_stuck_out_tongue_and_winking_eye
+import com.bro.brocast.R.id.button_crazy_face
+import com.bro.brocast.R.id.button_face_with_raised_eyebrow
+import com.bro.brocast.R.id.button_face_with_monocle
+import com.bro.brocast.R.id.button_nerd_face
+import com.bro.brocast.R.id.button_star_struck
+import com.bro.brocast.R.id.button_smiling_face_with_sunglasses
+import com.bro.brocast.R.id.button_partying_face
+import com.bro.brocast.R.id.button_smirking_face
+import com.bro.brocast.R.id.button_unamused_face
+import com.bro.brocast.R.id.button_disappointed_face
+import com.bro.brocast.R.id.button_pensive_face
+import com.bro.brocast.R.id.button_worried_face
+import com.bro.brocast.R.id.button_confused_face
 
 class FirstKeyboard: LinearLayout {
 
@@ -50,7 +69,7 @@ class FirstKeyboard: LinearLayout {
         val buttonIds = arrayOf(
             button_grinning_face,
             button_smiling_face_with_open_mouth,
-            button_smiling_face_with_open_mouth_and_smilinge_eyes,
+            button_smiling_face_with_open_mouth_and_smiling_eyes,
             button_grinning_face_with_smiling_eyes,
             button_smiling_face_with_open_mouth_and_closed_eyes,
             button_smiling_face_with_open_mouth_and_cold_sweat,
@@ -66,7 +85,26 @@ class FirstKeyboard: LinearLayout {
             button_smiling_face_with_heart_eyes,
             button_smiling_face_with_3_hearts,
             button_face_blowing_a_kiss,
-            button_kissing_face
+            button_kissing_face,
+            button_kissing_face_with_smiling_eyes,
+            button_kissing_face_with_closed_eyes,
+            button_face_savouring_delicious_food,
+            button_face_with_stuck_out_tongue,
+            button_face_with_stuck_out_tongue_and_closed_eyes,
+            button_face_with_stuck_out_tongue_and_winking_eye,
+            button_crazy_face,
+            button_face_with_raised_eyebrow,
+            button_face_with_monocle,
+            button_nerd_face,
+            button_star_struck,
+            button_smiling_face_with_sunglasses,
+            button_partying_face,
+            button_smirking_face,
+            button_unamused_face,
+            button_disappointed_face,
+            button_pensive_face,
+            button_worried_face,
+            button_confused_face
         )
 
         for (b in buttonIds) {
@@ -82,8 +120,8 @@ class FirstKeyboard: LinearLayout {
             button_smiling_face_with_open_mouth -> {
                 inputConnection!!.commitText(context.getString(R.string.smiling_face_with_open_mouth), 1)
             }
-            button_smiling_face_with_open_mouth_and_smilinge_eyes -> {
-                inputConnection!!.commitText(context.getString(R.string.smiling_face_with_open_mouth_and_smilinge_eyes), 1)
+            button_smiling_face_with_open_mouth_and_smiling_eyes -> {
+                inputConnection!!.commitText(context.getString(R.string.smiling_face_with_open_mouth_and_smiling_eyes), 1)
             }
             button_grinning_face_with_smiling_eyes -> {
                 inputConnection!!.commitText(context.getString(R.string.grinning_face_with_smiling_eyes), 1)
@@ -132,6 +170,63 @@ class FirstKeyboard: LinearLayout {
             }
             button_kissing_face -> {
                 inputConnection!!.commitText(context.getString(R.string.kissing_face), 1)
+            }
+            button_kissing_face_with_smiling_eyes -> {
+                inputConnection!!.commitText(context.getString(R.string.kissing_face_with_smiling_eyes), 1)
+            }
+            button_kissing_face_with_closed_eyes -> {
+                inputConnection!!.commitText(context.getString(R.string.kissing_face_with_closed_eyes), 1)
+            }
+            button_face_savouring_delicious_food -> {
+                inputConnection!!.commitText(context.getString(R.string.face_savouring_delicious_food), 1)
+            }
+            button_face_with_stuck_out_tongue -> {
+                inputConnection!!.commitText(context.getString(R.string.face_with_stuck_out_tongue), 1)
+            }
+            button_face_with_stuck_out_tongue_and_closed_eyes -> {
+                inputConnection!!.commitText(context.getString(R.string.face_with_stuck_out_tongue_and_closed_eyes), 1)
+            }
+            button_face_with_stuck_out_tongue_and_winking_eye -> {
+                inputConnection!!.commitText(context.getString(R.string.face_with_stuck_out_tongue_and_winking_eye), 1)
+            }
+            button_crazy_face -> {
+                inputConnection!!.commitText(context.getString(R.string.crazy_face), 1)
+            }
+            button_face_with_raised_eyebrow -> {
+                inputConnection!!.commitText(context.getString(R.string.face_with_raised_eyebrow), 1)
+            }
+            button_face_with_monocle -> {
+                inputConnection!!.commitText(context.getString(R.string.face_with_monocle), 1)
+            }
+            button_nerd_face -> {
+                inputConnection!!.commitText(context.getString(R.string.nerd_face), 1)
+            }
+            button_star_struck -> {
+                inputConnection!!.commitText(context.getString(R.string.star_struck), 1)
+            }
+            button_smiling_face_with_sunglasses -> {
+                inputConnection!!.commitText(context.getString(R.string.smiling_face_with_sunglasses), 1)
+            }
+            button_partying_face -> {
+                inputConnection!!.commitText(context.getString(R.string.partying_face), 1)
+            }
+            button_smirking_face -> {
+                inputConnection!!.commitText(context.getString(R.string.smirking_face), 1)
+            }
+            button_unamused_face -> {
+                inputConnection!!.commitText(context.getString(R.string.unamused_face), 1)
+            }
+            button_disappointed_face -> {
+                inputConnection!!.commitText(context.getString(R.string.disappointed_face), 1)
+            }
+            button_pensive_face -> {
+                inputConnection!!.commitText(context.getString(R.string.pensive_face), 1)
+            }
+            button_worried_face -> {
+                inputConnection!!.commitText(context.getString(R.string.worried_face), 1)
+            }
+            button_confused_face -> {
+                inputConnection!!.commitText(context.getString(R.string.confused_face), 1)
             }
         }
     }
