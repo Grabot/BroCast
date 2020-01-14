116ae5a05518f214882511c1bb381b575e40302c
Sander Kools <sanderkools@gmail.com>
2020-01-14 16:26:16 +0100

    adding cars! The emoji variants

--- app/src/main/java/com/bro/brocast/keyboards/FifthKeyboard.kt
+++ app/src/main/java/com/bro/brocast/keyboards/FifthKeyboard.kt
@@ -28,33 +28,99 @@ class FifthKeyboard: LinearLayout {
         init(context)
     }
 
-    var buttonSmile: Button? = null
-
-    val emoji_Smile = 0x1F604
-
     private var inputConnection: InputConnection? = null
 
     fun init(context: Context) {
         LayoutInflater.from(context).inflate(R.layout.keyboard_5, this, true)
 
-        buttonSmile = findViewById(R.id.button_smile_5)
-        buttonSmile!!.setOnClickListener(clickButtonListener)
+        val buttonIds = arrayOf(
+            R.id.button_automobile,
+            R.id.button_taxi,
+            R.id.button_sport_utility_vehicle,
+            R.id.button_bus,
+            R.id.button_trolleybus,
+            R.id.button_racing_car,
+            R.id.button_police_car,
+            R.id.button_ambulance,
+            R.id.button_fire_engine,
+            R.id.button_minibus,
+            R.id.button_delivery_truck,
+            R.id.button_articulated_lorry,
+            R.id.button_tractor,
+            R.id.button_kick_scooter,
+            R.id.button_bicycle,
+            R.id.button_motor_scooter,
+            R.id.button_motorcycle,
+            R.id.button_police_car_light,
+            R.id.button_oncoming_police_car
+        )
 
-        buttonSmile!!.text = getEmojiByUnicode(emoji_Smile)
+        for (b in buttonIds) {
+            findViewById<Button>(b).setOnClickListener(clickButtonListener)
+        }
     }
 
     private val clickButtonListener = OnClickListener { view ->
         when (view.getId()) {
-            R.id.button_smile_5 -> {
-                inputConnection!!.commitText(getEmojiByUnicode(emoji_Smile), 1)
+            R.id.button_automobile -> {
+                inputConnection!!.commitText(context.getString(R.string.automobile), 1)
+            }
+            R.id.button_taxi -> {
+                inputConnection!!.commitText(context.getString(R.string.taxi), 1)
+            }
+            R.id.button_sport_utility_vehicle -> {
+                inputConnection!!.commitText(context.getString(R.string.sport_utility_vehicle), 1)
+            }
+            R.id.button_bus -> {
+                inputConnection!!.commitText(context.getString(R.string.bus), 1)
+            }
+            R.id.button_trolleybus -> {
+                inputConnection!!.commitText(context.getString(R.string.trolleybus), 1)
+            }
+            R.id.button_racing_car -> {
+                inputConnection!!.commitText(context.getString(R.string.racing_car), 1)
+            }
+            R.id.button_police_car -> {
+                inputConnection!!.commitText(context.getString(R.string.police_car), 1)
+            }
+            R.id.button_ambulance -> {
+                inputConnection!!.commitText(context.getString(R.string.ambulance), 1)
+            }
+            R.id.button_fire_engine -> {
+                inputConnection!!.commitText(context.getString(R.string.fire_engine), 1)
+            }
+            R.id.button_minibus -> {
+                inputConnection!!.commitText(context.getString(R.string.minibus), 1)
+            }
+            R.id.button_delivery_truck -> {
+                inputConnection!!.commitText(context.getString(R.string.delivery_truck), 1)
+            }
+            R.id.button_articulated_lorry -> {
+                inputConnection!!.commitText(context.getString(R.string.articulated_lorry), 1)
+            }
+            R.id.button_tractor -> {
+                inputConnection!!.commitText(context.getString(R.string.tractor), 1)
+            }
+            R.id.button_kick_scooter -> {
+                inputConnection!!.commitText(context.getString(R.string.kick_scooter), 1)
+            }
+            R.id.button_bicycle -> {
+                inputConnection!!.commitText(context.getString(R.string.bicycle), 1)
+            }
+            R.id.button_motor_scooter -> {
+                inputConnection!!.commitText(context.getString(R.string.motor_scooter), 1)
+            }
+            R.id.button_motorcycle -> {
+                inputConnection!!.commitText(context.getString(R.string.motorcycle), 1)
+            }
+            R.id.button_police_car_light -> {
+                inputConnection!!.commitText(context.getString(R.string.police_car_light), 1)
+            }
+            R.id.button_oncoming_police_car -> {
+                inputConnection!!.commitText(context.getString(R.string.oncoming_police_car), 1)
             }
         }
     }
-
-    private fun getEmojiByUnicode(unicode: Int): String {
-        return String(Character.toChars(unicode))
-    }
-
 
     fun setInputConnection(ic: InputConnection) {
         inputConnection = ic
