7d5649bd2c7651f417e5b2146ab8902445aa6ffd
Sander Kools <sanderkools@gmail.com>
2020-01-15 16:07:15 +0100

    flags! the last of the tabs!

--- app/src/main/java/com/bro/brocast/keyboards/EighthKeyboard.kt
+++ app/src/main/java/com/bro/brocast/keyboards/EighthKeyboard.kt
@@ -28,33 +28,99 @@ class EighthKeyboard: LinearLayout {
         init(context)
     }
 
-    var buttonSmile: Button? = null
-
-    val emoji_Smile = 0x1F604
-
     private var inputConnection: InputConnection? = null
 
     fun init(context: Context) {
         LayoutInflater.from(context).inflate(R.layout.keyboard_8, this, true)
 
-        buttonSmile = findViewById(R.id.button_smile_8)
-        buttonSmile!!.setOnClickListener(clickButtonListener)
+        val buttonIds = arrayOf(
+            R.id.button_white_flag,
+            R.id.button_black_flag,
+            R.id.button_pirate_flag,
+            R.id.button_chequered_flag,
+            R.id.button_triangular_flag,
+            R.id.button_rainbow_flag,
+            R.id.button_united_nations,
+            R.id.button_afghanistan,
+            R.id.button_aland_islands,
+            R.id.button_albania,
+            R.id.button_algeria,
+            R.id.button_american_samoa,
+            R.id.button_andorra,
+            R.id.button_angola,
+            R.id.button_anguilla,
+            R.id.button_antarctica,
+            R.id.button_antigua_and_barbuda,
+            R.id.button_argentina,
+            R.id.button_armenia
+        )
 
-        buttonSmile!!.text = getEmojiByUnicode(emoji_Smile)
+        for (b in buttonIds) {
+            findViewById<Button>(b).setOnClickListener(clickButtonListener)
+        }
     }
 
     private val clickButtonListener = OnClickListener { view ->
         when (view.getId()) {
-            R.id.button_smile_8 -> {
-                inputConnection!!.commitText(getEmojiByUnicode(emoji_Smile), 1)
+            R.id.button_white_flag -> {
+                inputConnection!!.commitText(context.getString(R.string.white_flag), 1)
+            }
+            R.id.button_black_flag -> {
+                inputConnection!!.commitText(context.getString(R.string.black_flag), 1)
+            }
+            R.id.button_pirate_flag -> {
+                inputConnection!!.commitText(context.getString(R.string.pirate_flag), 1)
+            }
+            R.id.button_chequered_flag -> {
+                inputConnection!!.commitText(context.getString(R.string.chequered_flag), 1)
+            }
+            R.id.button_triangular_flag -> {
+                inputConnection!!.commitText(context.getString(R.string.triangular_flag), 1)
+            }
+            R.id.button_rainbow_flag -> {
+                inputConnection!!.commitText(context.getString(R.string.rainbow_flag), 1)
+            }
+            R.id.button_united_nations -> {
+                inputConnection!!.commitText(context.getString(R.string.united_nations), 1)
+            }
+            R.id.button_afghanistan -> {
+                inputConnection!!.commitText(context.getString(R.string.afghanistan), 1)
+            }
+            R.id.button_aland_islands -> {
+                inputConnection!!.commitText(context.getString(R.string.aland_islands), 1)
+            }
+            R.id.button_albania -> {
+                inputConnection!!.commitText(context.getString(R.string.albania), 1)
+            }
+            R.id.button_algeria -> {
+                inputConnection!!.commitText(context.getString(R.string.algeria), 1)
+            }
+            R.id.button_american_samoa -> {
+                inputConnection!!.commitText(context.getString(R.string.american_samoa), 1)
+            }
+            R.id.button_andorra -> {
+                inputConnection!!.commitText(context.getString(R.string.andorra), 1)
+            }
+            R.id.button_angola -> {
+                inputConnection!!.commitText(context.getString(R.string.angola), 1)
+            }
+            R.id.button_anguilla -> {
+                inputConnection!!.commitText(context.getString(R.string.anguilla), 1)
+            }
+            R.id.button_antarctica -> {
+                inputConnection!!.commitText(context.getString(R.string.antarctica), 1)
+            }
+            R.id.button_antigua_and_barbuda -> {
+                inputConnection!!.commitText(context.getString(R.string.antigua_and_barbuda), 1)
+            }
+            R.id.button_argentina -> {
+                inputConnection!!.commitText(context.getString(R.string.argentina), 1)
+            }
+            R.id.button_armenia -> {
+                inputConnection!!.commitText(context.getString(R.string.armenia), 1)
             }
         }
     }
-
-    private fun getEmojiByUnicode(unicode: Int): String {
-        return String(Character.toChars(unicode))
-    }
-
 
     fun setInputConnection(ic: InputConnection) {
         inputConnection = ic
