d171e643c784aeaa4372143e20d0be77864eb34b
Grabot <SanderKools@gmail.com>
2021-08-24 12:26:45 +0200

    added search possibility for friends in broup functionality

--- lib/views/add_broup.dart
+++ lib/views/add_broup.dart
@@ -7,6 +7,7 @@ import 'package:brocast/utils/shared.dart';
 import 'package:brocast/utils/utils.dart';
 import 'package:brocast/views/bro_home.dart';
 import 'package:brocast/views/signin.dart';
+import 'package:emoji_keyboard_flutter/emoji_keyboard_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:back_button_interceptor/back_button_interceptor.dart';
 
@@ -25,17 +26,25 @@ class _AddBroupState extends State<AddBroup> with WidgetsBindingObserver {
   GetBros getBros = new GetBros();
 
   List<BroBros> bros = [];
+  List<BroBros> shownBros = [];
+
+  TextEditingController bromotionController = new TextEditingController();
+  TextEditingController broNameController = new TextEditingController();
+
+  bool showEmojiKeyboard = false;
 
   @override
   void initState() {
     super.initState();
+    bromotionController.addListener(bromotionListener);
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      List<BroBros> brobros = BroList.instance.getBros();
-      if (brobros.isEmpty) {
+      List<BroBros> broBros = BroList.instance.getBros();
+      if (broBros.isEmpty) {
         searchBros(Settings.instance.getToken());
       } else {
         setState(() {
-          bros = brobros;
+          bros = broBros;
+          shownBros = broBros;
         });
       }
     });
@@ -48,6 +57,7 @@ class _AddBroupState extends State<AddBroup> with WidgetsBindingObserver {
       if (!(val is String)) {
         setState(() {
           bros = val;
+          shownBros = val;
           BroList.instance.setBros(bros);
         });
       } else {
@@ -93,9 +103,27 @@ class _AddBroupState extends State<AddBroup> with WidgetsBindingObserver {
         ]);
   }
 
+  bromotionListener() {
+    bromotionController.selection =
+        TextSelection.fromPosition(TextPosition(offset: 0));
+    String fullText = bromotionController.text;
+    String lastEmoji = fullText.characters.skip(1).string;
+    if (lastEmoji != "") {
+      String newText = bromotionController.text.replaceFirst(lastEmoji, "");
+      bromotionController.text = newText;
+    }
+    onChangedBroNameField(broNameController.text, bromotionController.text);
+  }
+
   void backButtonFunctionality() {
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => BroCastHome()));
+    if (showEmojiKeyboard) {
+      setState(() {
+        showEmojiKeyboard = false;
+      });
+    } else {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => BroCastHome()));
+    }
   }
 
   void onSelect(BuildContext context, int item) {
@@ -120,14 +148,56 @@ class _AddBroupState extends State<AddBroup> with WidgetsBindingObserver {
   }
 
   Widget broList() {
-    return bros.isNotEmpty
+    return shownBros.isNotEmpty
         ? ListView.builder(
         shrinkWrap: true,
-        itemCount: bros.length,
+        itemCount: shownBros.length,
         itemBuilder: (context, index) {
-          return BroTileAddBroup(broBros: bros[index]);
+          return BroTileAddBroup(broBros: shownBros[index]);
         })
         : Container();
+  }
+
+  void onChangedBroNameField(String typedText, String emojiField) {
+    if (emojiField.isEmpty && typedText.isNotEmpty) {
+      shownBros = bros.where((element) =>
+          element.chatName.toLowerCase()
+              .contains(typedText.toLowerCase())).toList();
+    } else if (emojiField.isNotEmpty && typedText.isEmpty) {
+      shownBros = bros.where((element) =>
+          element.chatName.toLowerCase()
+              .contains(emojiField)).toList();
+    } else if (emojiField.isNotEmpty && typedText.isNotEmpty) {
+      shownBros = bros.where((element) =>
+          element.chatName.toLowerCase()
+              .contains(typedText.toLowerCase()) &&
+              element.chatName.toLowerCase()
+                  .contains(emojiField)).toList();
+    } else {
+      // both empty
+      shownBros = bros;
+    }
+    setState(() {
+    });
+  }
+
+  void onTapTextField() {
+    if (showEmojiKeyboard) {
+      setState(() {
+        showEmojiKeyboard = false;
+      });
+    }
+  }
+
+  void onTapEmojiField() {
+    if (!showEmojiKeyboard) {
+      // We add a quick delay, this is to ensure that the keyboard is gone at this point.
+      Future.delayed(Duration(milliseconds: 100)).then((value) {
+        setState(() {
+          showEmojiKeyboard = true;
+        });
+      });
+    }
   }
 
   @override
@@ -141,12 +211,12 @@ class _AddBroupState extends State<AddBroup> with WidgetsBindingObserver {
               Container(
                 alignment: Alignment.centerLeft,
                 child: Text(
-                  "bros",
+                  "Participants in bro group",
                   style: simpleTextStyle()
                 ),
               ),
               Container(
-                  height: 40
+                  height: 60
               ),
               SizedBox(
                 height: 10
@@ -195,10 +265,63 @@ class _AddBroupState extends State<AddBroup> with WidgetsBindingObserver {
                   ]
                 ),
               ),
+              SizedBox(height: 20),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                    "Search for your bro",
+                    style: simpleTextStyle()
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: TextFormField(
+                        onTap: () {
+                          onTapTextField();
+                        },
+                        onChanged: (text) {
+                          onChangedBroNameField(text, bromotionController.text);
+                        },
+                        controller: broNameController,
+                        textAlign: TextAlign.center,
+                        style: simpleTextStyle(),
+                        decoration: textFieldInputDecoration("Bro name"),
+                      ),
+                    ),
+                    SizedBox(width: 50),
+                    Expanded(
+                      flex: 1,
+                      child: TextFormField(
+                        onTap: () {
+                          onTapEmojiField();
+                        },
+                        controller: bromotionController,
+                        style: simpleTextStyle(),
+                        textAlign: TextAlign.center,
+                        decoration: textFieldInputDecoration("😀"),
+                        readOnly: true,
+                        showCursor: true,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               SizedBox(
                   height: 10
               ),
-              broList(),
+              Expanded(child: broList()),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: EmojiKeyboard(
+                    bromotionController: bromotionController,
+                    emojiKeyboardHeight: 300,
+                    showEmojiKeyboard: showEmojiKeyboard,
+                    darkMode: Settings.instance.getEmojiKeyboardDarkMode()),
+              ),
             ],
           )
       ),
@@ -219,9 +342,24 @@ class _BroTileAddBroupState extends State<BroTileAddBroup> {
 
   void selectBro(BuildContext context) {
     print("bro selected");
+    setState(() {
+      isChecked = !isChecked;
+    });
   }
 
-  bool checkedValue = false;
+  bool isChecked = false;
+
+  Color getColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.pressed,
+      MaterialState.hovered,
+      MaterialState.focused,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return Colors.blue;
+    }
+    return Colors.red;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -236,6 +374,16 @@ class _BroTileAddBroupState extends State<BroTileAddBroup> {
           children: [
             Container(
               width: 50,
+              child: Checkbox(
+                checkColor: Colors.white,
+                fillColor: MaterialStateProperty.resolveWith(getColor),
+                value: isChecked,
+                onChanged: (bool value) {
+                  setState(() {
+                    isChecked = value;
+                  });
+                },
+              ),
             ),
             Container(
               width: MediaQuery.of(context).size.width-50,
