605f921cc912ab72809b53e888f355424f248db8
Grabot <SanderKools@gmail.com>
2025-01-24 22:19:43 +0100

    revert back to basics

--- lib/constants/route_paths.dart
+++
@@ -1,4 +0,0 @@
-const String BroRoute = 'Bro';
-const String BroupRoute = 'Broup';
-const String HomeRoute = 'Home';
-const String OpeningRoute = 'Opening';
--- lib/utils/locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:brocast/services/navigation_service.dart';
-
-GetIt locator = GetIt.instance;
-
-void setupLocator() {
-  locator.registerLazySingleton(() => NavigationService());
-}
