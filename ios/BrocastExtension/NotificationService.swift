0f378e7d5b66fbc96067e0bcfd938a773e605d47
Grabot <SanderKools@gmail.com>
2025-04-04 12:36:35 -0700

    attempt to fix ios

---
+++ ios/BrocastExtension/NotificationService.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationService.swift
+//  BrocastExtension
+//
+//  Created by Zwaar on 04/04/2025.
+//
+import UserNotifications
+import awesome_notifications_fcm
+
+@available(iOS 10.0, *)
+class NotificationService: DartAwesomeServiceExtension {
+
+}
