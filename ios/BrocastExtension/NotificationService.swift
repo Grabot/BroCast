de55647128fa8d64fb5605c82c3a73d9d6289286
Grabot <SanderKools@gmail.com>
2025-04-07 09:46:17 +0200

    found some notification copy paste stuff

--- ios/BrocastExtension/NotificationService.swift
+++ ios/BrocastExtension/NotificationService.swift
@@ -9,5 +9,22 @@ import awesome_notifications_fcm
 
 @available(iOS 10.0, *)
 class NotificationService: DartAwesomeServiceExtension {
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
 
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+
+        super.didReceive(request, withContentHandler: { content in
+                    self.bestAttemptContent = (content.mutableCopy() as? UNMutableNotificationContent)
+
+                    guard let bestAttemptContent = self.bestAttemptContent else {
+                        return
+                    }
+
+                    bestAttemptContent.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "common_notification_sound.aiff"))
+
+                    contentHandler(bestAttemptContent)
+                })
+    }
 }
