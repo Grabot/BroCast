babb755bb31e395576ea1269cc2b4957db1c2445
Grabot <SanderKools@gmail.com>
2025-04-07 09:36:14 +0200

    some swift changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import Flutter
 import UIKit
 import awesome_notifications
+import awesome_notifications_fcm
+import flutter_secure_storage
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,16 +11,21 @@ import awesome_notifications
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
     // This function register the desired plugins to be used within a notification background action
     SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in
-        SwiftAwesomeNotificationsPlugin.register(
-            with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
+      SwiftAwesomeNotificationsPlugin.register(
+        with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
+      FlutterSecureStoragePlugin.register(
+        with: registry.registrar(forPlugin: "io.flutter.plugins.fluttersecurestorage.FlutterSecureStoragePlugin")!)
     }
 
     // This function register the desired plugins to be used within silent push notifications
     SwiftAwesomeNotificationsFcmPlugin.setPluginRegistrantCallback { registry in
-        SwiftAwesomeNotificationsPlugin.register(
-            with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
+      SwiftAwesomeNotificationsPlugin.register(
+        with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
+      FlutterSecureStoragePlugin.register(
+        with: registry.registrar(forPlugin: "io.flutter.plugins.fluttersecurestorage.FlutterSecureStoragePlugin")!)
     }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
