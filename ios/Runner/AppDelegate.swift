c964f3cc6adf9b6694896676ee36540dc208b93a
Grabot <SanderKools@gmail.com>
2025-10-07 20:18:53 +0200

    adding it back slowely ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,33 +4,36 @@ import GoogleMaps
 import awesome_notifications
 import awesome_notifications_fcm
 import flutter_secure_storage
+import flutter_sharing_intent
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-
-    // This function register the desired plugins to be used within a notification background action
-    SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in
-      SwiftAwesomeNotificationsPlugin.register(
-        with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
-      FlutterSecureStoragePlugin.register(
-        with: registry.registrar(forPlugin: "io.flutter.plugins.fluttersecurestorage.FlutterSecureStoragePlugin")!)
+    override func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        
+        SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback {
+            registry in SwiftAwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
+            FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "io.flutter.plugins.fluttersecurestorage.FlutterSecureStoragePlugin")!)
+        }
+        
+        SwiftAwesomeNotificationsFcmPlugin.setPluginRegistrantCallback {
+            registry in SwiftAwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
+            FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "io.flutter.plugins.fluttersecurestorage.FlutterSecureStoragePlugin")!)
+        }
+        
+        GMSServices.provideAPIKey("<My api key>")
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-
-    // This function register the desired plugins to be used within silent push notifications
-    SwiftAwesomeNotificationsFcmPlugin.setPluginRegistrantCallback { registry in
-      SwiftAwesomeNotificationsPlugin.register(
-        with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
-      FlutterSecureStoragePlugin.register(
-        with: registry.registrar(forPlugin: "io.flutter.plugins.fluttersecurestorage.FlutterSecureStoragePlugin")!)
+    
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        let sharingIntent = SwiftFlutterSharingIntentPlugin.instance
+        
+        if sharingIntent.hasSameSchemePrefix(url: url) {
+            return sharingIntent.application(app, open: url, options: options)
+        }
+        
+        return super.application(app, open: url, options:options)
     }
-    // TODO: Add from config?
-    GMSServices.provideAPIKey("<My api key>")
-
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 }
